6c1c8cfaa27f2895260a1d116c80a6c240f68085
Laís Brum <laisbrme@gmail.com>
2025-06-29 17:53:07 -0300

    :test_tube: feat(test_login): adiciona teste automatizado CT-03 para fluxo de login

--- Desafio_2/testes_automatizados/test_login.py
+++ Desafio_2/testes_automatizados/test_login.py
@@ -20,6 +20,15 @@ def test_login_CT02(browser, path_login):
     time.sleep(5)
     assert "login" in browser.current_url or "Login" in browser.page_source
 
+# Caso de teste CT-03:
+def test_login_CT03(browser, path_login):
+    browser.get(path_login)  # Atualize o caminho conforme necessário
+    browser.find_element("id", "senha").send_keys("admin@123")
+    time.sleep(1)
+    browser.find_element("id", "btn-entrar").click()
+    time.sleep(5)
+    assert "login" in browser.current_url or "Login" in browser.page_source
+
 def test_login_sucesso(browser, path_login):
     browser.get(path_login)  # Atualize o caminho conforme necessário
     browser.find_element("id", "email").send_keys("admin@admin.com")
