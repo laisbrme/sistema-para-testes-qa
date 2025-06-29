a06fea8ff2f8fc45f84f4f857f581795b2bcaf78
Laís Brum <laisbrme@gmail.com>
2025-06-29 17:37:28 -0300

    :test_tube: feat(test_login): adiciona teste automatizado CT-01 para fluxo de login

--- Desafio_2/testes_automatizados/test_login.py
+++ Desafio_2/testes_automatizados/test_login.py
@@ -3,6 +3,14 @@ Função para testes automatizados para o fluxo de login do sistema.
 '''
 import time
 
+# Caso de teste CT-01:
+def test_login_CT01(browser, path_login):
+    browser.get(path_login)  # Atualize o caminho conforme necessário
+    time.sleep(1)
+    browser.find_element("id", "btn-entrar").click()
+    time.sleep(5)
+    assert "login" in browser.current_url or "Login" in browser.page_source
+
 def test_login_sucesso(browser, path_login):
     browser.get(path_login)  # Atualize o caminho conforme necessário
     browser.find_element("id", "email").send_keys("admin@admin.com")
