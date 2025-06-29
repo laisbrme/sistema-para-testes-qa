545fe3bd1b4d91ae5a52d269a122161d1ebd53d1
Laís Brum <laisbrme@gmail.com>
2025-06-29 12:31:25 -0300

    :test_tube: test(automacao): adiciona test_login.py com teste automatizado para fluxo de login

---
+++ Desafio_2/testes_automatizados/test_login.py
@@ -0,0 +1,10 @@
+'''
+Função para testes automatizados para o fluxo de login do sistema.
+'''
+
+def test_login_sucesso(browser, path_login):
+    browser.get(path_login)  # Atualize o caminho conforme necessário
+    browser.find_element("id", "email").send_keys("admin@admin.com")
+    browser.find_element("id", "senha").send_keys("admin@123")
+    browser.find_element("id", "btnEntrar").click()
+    assert "produtos" in browser.current_url or "Produtos" in browser.page_source
