b30f70d1f883de2f8e9b584dfa1989fde1f85991
Laís Brum <laisbrme@gmail.com>
2025-06-29 16:28:29 -0300

    :alarm_clock: chore(test_login): adiciona time.sleep() para aguardar carregamento dos elementos durante o teste de login

--- Desafio_2/testes_automatizados/test_login.py
+++ Desafio_2/testes_automatizados/test_login.py
@@ -1,10 +1,14 @@
 '''
 Função para testes automatizados para o fluxo de login do sistema.
 '''
+import time
 
 def test_login_sucesso(browser, path_login):
     browser.get(path_login)  # Atualize o caminho conforme necessário
     browser.find_element("id", "email").send_keys("admin@admin.com")
+    time.sleep(1)
     browser.find_element("id", "senha").send_keys("admin@123")
-    browser.find_element("id", "btnEntrar").click()
+    time.sleep(1)
+    browser.find_element("id", "btn-entrar").click()
+    time.sleep(5)
     assert "produtos" in browser.current_url or "Produtos" in browser.page_source
