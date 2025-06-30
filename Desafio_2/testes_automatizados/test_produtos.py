329b58098f1db7e14731620acb502ebf27035334
Laís Brum <laisbrme@gmail.com>
2025-06-29 22:34:37 -0300

    :test_tube: feat(test_produto): adiciona teste automatizado CT-12 para fluxo de login

--- Desafio_2/testes_automatizados/test_produtos.py
+++ Desafio_2/testes_automatizados/test_produtos.py
@@ -114,3 +114,33 @@ def test_produto_CT11(browser, path_login):
     browser.find_element("id", "btn-sair").click()
     # time.sleep(3)
     assert "2025-13-32" not in browser.page_source
+
+# Caso de teste CT-12:
+def test_produto_CT12(browser, path_login):
+    browser.get(path_login)  # Atualize o caminho conforme necessário
+    browser.find_element("id", "email").send_keys("admin@admin.com")
+    browser.find_element("id", "senha").send_keys("admin@123")
+    browser.find_element("id", "btn-entrar").click()
+    # time.sleep(1)
+
+    quantidade = "XX"
+    valor = "XX"
+
+    browser.find_element("id", "btn-adicionar").click()
+    browser.find_element("id", "btn-adicionar").click()
+    # time.sleep(1)
+    browser.find_element("id", "codigo").send_keys("001")
+    # time.sleep(1)
+    browser.find_element("id", "nome").send_keys("Produto Teste")
+    # time.sleep(1)
+    browser.find_element("id", "quantidade").send_keys(quantidade)
+    # time.sleep(1)
+    browser.find_element("id", "valor").send_keys(valor)
+    # time.sleep(1)
+    browser.find_element("id", "data").send_keys("25062025")
+    # time.sleep(1)
+    browser.find_element("id", "btn-salvar").click()
+    # time.sleep(1)
+    browser.find_element("id", "btn-sair").click()
+    # time.sleep(3)
+    assert not (type(quantidade) == str) or not (type(valor) == str)
