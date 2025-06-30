7253e4806fb7e2a8bfdf195ffed7fd64fa25272d
Laís Brum <laisbrme@gmail.com>
2025-06-29 22:10:35 -0300

    :test_tube: feat(test_produto): adiciona teste automatizado CT-10 para fluxo de login

--- Desafio_2/testes_automatizados/test_produtos.py
+++ Desafio_2/testes_automatizados/test_produtos.py
@@ -49,11 +49,41 @@ def test_produto_CT09(browser, path_login):
     browser.find_element("id", "email").send_keys("admin@admin.com")
     browser.find_element("id", "senha").send_keys("admin@123")
     browser.find_element("id", "btn-entrar").click()
-    time.sleep(1)
+    # time.sleep(1)
 
     browser.find_element("id", "btn-adicionar").click()
     browser.find_element("id", "btn-adicionar").click()
-    time.sleep(1)
+    # time.sleep(1)
     browser.find_element("id", "btn-salvar").click()
-    time.sleep(3)
+    # time.sleep(3)
     assert "Todos os campos são obrigatórios para o cadastro!" in browser.page_source
+    
+# Caso de teste CT-10:
+def test_produto_CT10(browser, path_login):
+    browser.get(path_login)  # Atualize o caminho conforme necessário
+    browser.find_element("id", "email").send_keys("admin@admin.com")
+    browser.find_element("id", "senha").send_keys("admin@123")
+    browser.find_element("id", "btn-entrar").click()
+    # time.sleep(1)
+
+    quantidade = -10
+    valor = -15
+
+    browser.find_element("id", "btn-adicionar").click()
+    browser.find_element("id", "btn-adicionar").click()
+    # time.sleep(1)
+    browser.find_element("id", "codigo").send_keys("001")
+    # time.sleep(1)
+    browser.find_element("id", "nome").send_keys("Produto Teste")
+    # time.sleep(1)
+    browser.find_element("id", "quantidade").send_keys(quantidade)
+    # time.sleep(1)
+    browser.find_element("id", "valor").send_keys(valor)
+    # time.sleep(1)
+    browser.find_element("id", "data").send_keys("25062025")
+    # time.sleep(1)
+    browser.find_element("id", "btn-salvar").click()
+    # time.sleep(1)
+    browser.find_element("id", "btn-sair").click()
+    # time.sleep(3)
+    assert not (quantidade < 0) or not (valor < 0)
