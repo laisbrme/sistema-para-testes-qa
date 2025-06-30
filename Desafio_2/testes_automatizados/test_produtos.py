f3fc8991332295aed158fdafd30eea1969bef2e2
Laís Brum <laisbrme@gmail.com>
2025-06-29 23:09:17 -0300

    :test_tube: feat(test_produto): adiciona teste automatizado CT-14 para fluxo de cadastro de produto

--- Desafio_2/testes_automatizados/test_produtos.py
+++ Desafio_2/testes_automatizados/test_produtos.py
@@ -169,7 +169,7 @@ def test_produto_CT13(browser, path_login):
     browser.find_element("id", "btn-salvar").click()
     # time.sleep(1)
     browser.find_element("id", "btn-sair").click()
-    time.sleep(3)
+    # time.sleep(3)
 
     # time.sleep(1)
     browser.find_element("xpath", "//button[text()='Entrar']").click()
@@ -178,3 +178,33 @@ def test_produto_CT13(browser, path_login):
     # time.sleep(1)
     assert "Produto Teste Modificado" in browser.page_source
 
+# Caso de teste CT-14:
+def test_produto_CT14(browser, path_login):
+    browser.get(path_login)  # Atualize o caminho conforme necessário
+    browser.find_element("id", "email").send_keys("admin@admin.com")
+    browser.find_element("id", "senha").send_keys("admin@123")
+    browser.find_element("id", "btn-entrar").click()
+    # time.sleep(1)
+
+    browser.find_element("id", "btn-adicionar").click()
+    browser.find_element("id", "btn-adicionar").click()
+    # time.sleep(1)
+    browser.find_element("id", "codigo").send_keys("001")
+    # time.sleep(1)
+    browser.find_element("id", "nome").send_keys("Produto Teste")
+    # time.sleep(1)
+    browser.find_element("id", "quantidade").send_keys("10")
+    # time.sleep(1)
+    browser.find_element("id", "valor").send_keys("99.99")
+    # time.sleep(1)
+    browser.find_element("id", "data").send_keys("25062025")
+    # time.sleep(1)
+    browser.find_element("id", "btn-salvar").click()
+    # time.sleep(1)
+    browser.find_element("id", "btn-sair").click()
+    time.sleep(3)
+
+    # time.sleep(1)
+    browser.find_element("xpath", "//button[text()='Excluir']").click()
+    # time.sleep(1)
+    assert "Produto Teste" not in browser.page_source
